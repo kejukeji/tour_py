fe0c5b4aa4e2855ae920e7d56c8a5f028b845f79
eXthen <exthen@gmail.com>
2013-10-25 17:47:13 +0800

    为测试服务器做准备

--- tour/setting/server.py
+++ tour/setting/server.py
@@ -12,3 +12,8 @@ SECRET_KEY = EX_SECRET_KEY_SERVER  # session相关的密匙
 SQLALCHEMY_DATABASE_URI = 'mysql://' + EX_DB_USER + ':' + EX_DB_PASSWORD + '@127.0.0.1:3306/' \
                           + EX_DB_NAME + '?charset=utf8'  # 连接的数据库
 SQLALCHEMY_ECHO = False  # 是否显示SQL语句
+
+# 折扣图片
+TOUR_PICTURE_UPLOAD_FOLDER = '/static/system/tour_picture'  # 运行目录的相对目录，URL获取图片的路径
+TOUR_PICTURE_BASE_PATH = '/var/www/tour/tour'  # pub运行文件的目录，图片的绝对路径使用
+TOUR_PICTURE_ALLOWED_EXTENSION = ('png', 'jpg', 'jpeg', 'gif')  # 允许的拓展名
