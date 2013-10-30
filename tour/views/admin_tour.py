30ae470beab15d4261dca741cc3303669d7a5904
eXthen <exthen@gmail.com>
2013-10-30 22:08:40 +0800

    test1

--- tour/views/admin_tour.py
+++ tour/views/admin_tour.py
@@ -42,7 +42,7 @@ class TourView(ModelView):
     )
     column_descriptions = dict(
         order_max=u'最大订购人数，如果没有限制，默认为0',
-        ordered=u'已订购人数，首先默认为0，然后系统自动添加',
+        ordered=u'已订购人数，首先默认为0，打电话之后手动添加',
         rank=u'排序，值越大，可以约靠前，最前面四个作为广告放在最前面，默认为0',
         stopped=u'如果这个订购取消，可以勾选'
     )
